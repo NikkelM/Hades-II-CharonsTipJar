3f52e2b64d7da61ce3432819340f7f81f07f88f3
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-12-05 20:26:38 +0000

    Updated used group for proper Z-Buffer location

--- src/Scripts/CharonsTipJar.lua
+++ src/Scripts/CharonsTipJar.lua
@@ -1,8 +1,8 @@
 local mod = modutil.mod.Mod.Register(_PLUGIN.guid)
 
+-- For debugging - Erebus
 table.insert(game.RoomSetData.F.F_PreBoss01.StartUnthreadedEvents,
 	{
-		-- TODO: Spawn mailbox in final preboss rooms
 		FunctionName = _PLUGIN.guid .. '.' .. 'SpawnCharonsTipJar',
 		Args = { Location = "F_PreBoss01" }
 	})
@@ -23,27 +23,35 @@ table.insert(game.RoomSetData.P.P_PreBoss01.StartUnthreadedEvents,
 
 function mod.SpawnCharonsTipJar(source, args)
 	-- Defines the starting point for the spawn, against which the offset is applied. This is Charon
-	-- TODO: This is in F_PreBoss01 -- check other rooms
+	-- TODO: This is the ID in F_PreBoss01 -- check other rooms
 	local spawnId = 561301
 	-- Copies the mailbox item
 	local tipJar = game.DeepCopyTable(game.HubRoomData.Hub_Main.ObstacleData[583652])
 
-	tipJar.OnUsedFunctionName = _PLUGIN.guid .. '.' .. 'TipCharon'
-
-	-- TODO: If the player has no money when approaching Charon, use a different useText
-	tipJar.UseText = "ModsNikkelMCharonsTipJar_TipJarUseText"
 	tipJar.ObjectId = SpawnObstacle({
 		Name = "SupplyDropObject",
-		-- TODO: Mailbox is currently always behind Melinoe, should be in front of her model if she is standing behind it. Might be able to find the group the store items use?
-		Group = "FX_Terrain",
+		Group = "Standing",
 		DestinationId = spawnId,
 		AttachedTable = tipJar,
 		OffsetX = 0,
 		OffsetY = 300,
 	})
 	tipJar.ActivateIds = { tipJar.ObjectId }
-	-- Remove the default setup events inherited from the mailbox
+
+	-- Overwrite some default values
+	-- TODO: If the player has no money when approaching Charon, use a different useText, like below
+	-- if mailboxStatus.TimeRemaining >= 1 then
+	-- 	source.UseText = "UseMailbox_DeliveryPending"
+	-- 	source.OnUsedFunctionName = "UseMailboxDeliveryPendingPresentation"
+	-- else
+	-- 	source.UseText = "UseMailbox_DeliveryReady"
+	-- 	source.OnUsedFunctionName = "UseMailboxDeliveryReady"
+	-- end
+	tipJar.UseText = "ModsNikkelMCharonsTipJar_TipJarUseText"
 	tipJar.SetupEvents = {}
+	tipJar.InteractDistance = 125
+	tipJar.OnUsedFunctionName = _PLUGIN.guid .. '.' .. 'TipCharon'
+
 	SetScale({ Id = tipJar.ObjectId, Fraction = 0.2 })
 	game.SetupObstacle(tipJar)
 	AddToGroup({ Id = tipJar.ObjectId, Name = "ModsNikkelMCharonsTipJar.TipJar" })
